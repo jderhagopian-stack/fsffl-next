a0528e07b77286f7c91b55386835af2ad47043e7
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-05 09:13:18 -0400

    NEXT-3 export FantasyCalc source adapter

--- src/fsffl/value/sources/__init__.py
+++ src/fsffl/value/sources/__init__.py
@@ -1,9 +1,12 @@
 from .dynastydealer import DynastyDealerImportResult, normalize_dynastydealer_values
 from .dynastyprocess import DynastyProcessImportResult, normalize_dynastyprocess_values
+from .fantasycalc import FantasyCalcImportResult, normalize_fantasycalc_values
 
 __all__ = [
     "DynastyDealerImportResult",
     "DynastyProcessImportResult",
+    "FantasyCalcImportResult",
     "normalize_dynastydealer_values",
     "normalize_dynastyprocess_values",
+    "normalize_fantasycalc_values",
 ]
