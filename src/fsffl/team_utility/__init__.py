217f6aebcd68d2ddca26b30e6701402292291000
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-05 11:12:14 -0400

    NEXT-4: export competitive simulation contracts

--- src/fsffl/team_utility/__init__.py
+++ src/fsffl/team_utility/__init__.py
@@ -1,10 +1,26 @@
 from .lineup import marginal_lineup_impact, optimize_team_lineup
 from .models import LineupAssignment, MarginalLineupImpact, OptimizedTeamLineup
+from .simulation import (
+    RegularSeasonSimulationInput,
+    RegularSeasonSimulationResult,
+    ScheduledMatchup,
+    ScoringDistributionKind,
+    TeamCompetitiveOutcome,
+    TeamScoringDistribution,
+    simulate_regular_season,
+)
 
 __all__ = [
     "LineupAssignment",
     "MarginalLineupImpact",
     "OptimizedTeamLineup",
+    "RegularSeasonSimulationInput",
+    "RegularSeasonSimulationResult",
+    "ScheduledMatchup",
+    "ScoringDistributionKind",
+    "TeamCompetitiveOutcome",
+    "TeamScoringDistribution",
     "marginal_lineup_impact",
     "optimize_team_lineup",
+    "simulate_regular_season",
 ]
