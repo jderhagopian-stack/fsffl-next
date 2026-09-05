b2450efa4bc3855b1a9fd5d9a5ae5b4efce04422
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-05 14:15:26 -0400

    NEXT-7: export analytics context contracts

---
+++ src/fsffl/analytics/__init__.py
@@ -0,0 +1,15 @@
+from .models import (
+    AnalyticsContext,
+    AnalyticsWarning,
+    AnalyticsWarningKind,
+    ModelLineageEntry,
+    canonical_analytics_json,
+)
+
+__all__ = [
+    "AnalyticsContext",
+    "AnalyticsWarning",
+    "AnalyticsWarningKind",
+    "ModelLineageEntry",
+    "canonical_analytics_json",
+]
