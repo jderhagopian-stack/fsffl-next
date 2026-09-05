09ebcd5798996baac87fa75d823373d8c277356c
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-05 11:07:51 -0400

    NEXT-4: export team utility foundation

---
+++ src/fsffl/team_utility/__init__.py
@@ -0,0 +1,10 @@
+from .lineup import marginal_lineup_impact, optimize_team_lineup
+from .models import LineupAssignment, MarginalLineupImpact, OptimizedTeamLineup
+
+__all__ = [
+    "LineupAssignment",
+    "MarginalLineupImpact",
+    "OptimizedTeamLineup",
+    "marginal_lineup_impact",
+    "optimize_team_lineup",
+]
