17dbdc7690eea3ecac5ffe4fd3163c696ef9c7c4
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-04 21:45:24 -0400

    NEXT-2 expose historical projection adapters

--- src/fsffl/forecast/adapters/__init__.py
+++ src/fsffl/forecast/adapters/__init__.py
@@ -1,3 +1,15 @@
+from .espn import ESPNProjectionRow, snapshots_from_espn_rows
+from .fantasypros import FantasyProsProjectionRow, snapshots_from_fantasypros_rows
 from .fftoday import FFTodayProjectionRow, snapshots_from_fftoday_rows
+from .tabular import RawStatProjectionRow, snapshots_from_raw_stat_rows
 
-__all__ = ["FFTodayProjectionRow", "snapshots_from_fftoday_rows"]
+__all__ = [
+    "ESPNProjectionRow",
+    "FantasyProsProjectionRow",
+    "FFTodayProjectionRow",
+    "RawStatProjectionRow",
+    "snapshots_from_espn_rows",
+    "snapshots_from_fantasypros_rows",
+    "snapshots_from_fftoday_rows",
+    "snapshots_from_raw_stat_rows",
+]
