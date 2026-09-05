3b78a1b504b889ada32d4bacaa33b75fc3d1e60e
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-05 13:44:51 -0400

    Export NEXT-6 opportunity lifecycle contracts

---
+++ src/fsffl/opportunity/__init__.py
@@ -0,0 +1,19 @@
+from .models import (
+    ActionAuthority,
+    CandidateReason,
+    DiscoveryStatus,
+    EvidenceCompleteness,
+    OpportunityCandidate,
+    OpportunityKind,
+    derive_action_authority,
+)
+
+__all__ = [
+    "ActionAuthority",
+    "CandidateReason",
+    "DiscoveryStatus",
+    "EvidenceCompleteness",
+    "OpportunityCandidate",
+    "OpportunityKind",
+    "derive_action_authority",
+]
