4462d4b30347ec284c9347fee4edba3c42fc8eea
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-05 09:01:54 -0400

    NEXT-3 export Dynasty Dealer source adapter

--- src/fsffl/value/sources/__init__.py
+++ src/fsffl/value/sources/__init__.py
@@ -1,6 +1,9 @@
+from .dynastydealer import DynastyDealerImportResult, normalize_dynastydealer_values
 from .dynastyprocess import DynastyProcessImportResult, normalize_dynastyprocess_values
 
 __all__ = [
+    "DynastyDealerImportResult",
     "DynastyProcessImportResult",
+    "normalize_dynastydealer_values",
     "normalize_dynastyprocess_values",
 ]
