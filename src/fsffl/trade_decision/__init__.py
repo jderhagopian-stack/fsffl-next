14a07a08cf75abb1d24dc6c8dae58a188d1810ed
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-05 13:11:05 -0400

    NEXT-5: export bilateral evaluation contract

--- src/fsffl/trade_decision/__init__.py
+++ src/fsffl/trade_decision/__init__.py
@@ -1,9 +1,17 @@
+from .evaluation import (
+    BilateralTradeEvaluation,
+    TradeSideEvaluation,
+    evaluate_bilateral_trade_deltas,
+)
 from .models import BilateralTradeProposal, TradeLeg
 from .scenario import AppliedTradeScenario, apply_bilateral_trade
 
 __all__ = [
     "AppliedTradeScenario",
+    "BilateralTradeEvaluation",
     "BilateralTradeProposal",
     "TradeLeg",
+    "TradeSideEvaluation",
     "apply_bilateral_trade",
+    "evaluate_bilateral_trade_deltas",
 ]
