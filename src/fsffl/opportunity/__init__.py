248c3e07436e7289a575ede0edf9175f28406415
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-05 14:02:25 -0400

    NEXT-6: export Pareto opportunity ordering

--- src/fsffl/opportunity/__init__.py
+++ src/fsffl/opportunity/__init__.py
@@ -14,6 +14,14 @@ from .models import (
     OpportunityKind,
     derive_action_authority,
 )
+from .ordering import (
+    ObjectiveDirection,
+    OpportunityObjective,
+    OrderedOpportunityPoint,
+    authority_tier,
+    dominates,
+    pareto_front,
+)
 from .trade_evaluation import candidate_from_trade_evaluation
 from .trade_universe import (
     TeamTradeInventory,
@@ -32,18 +40,24 @@ __all__ = [
     "DiscoveryStatus",
     "EvidenceCompleteness",
     "FrontierNeighbor",
+    "ObjectiveDirection",
     "OpportunityCandidate",
     "OpportunityKind",
+    "OpportunityObjective",
+    "OrderedOpportunityPoint",
     "TeamTradeInventory",
     "TradePackageSeed",
     "TradeSearchBounds",
+    "authority_tier",
     "build_team_trade_inventory",
     "candidate_from_trade_evaluation",
     "canonical_frontier_point_id",
     "canonical_package_id",
     "derive_action_authority",
+    "dominates",
     "enumerate_counterparties",
     "enumerate_trade_packages",
     "expand_adjacent_packages",
     "make_frontier_point",
+    "pareto_front",
 ]
