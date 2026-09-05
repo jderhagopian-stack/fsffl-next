b9eb0965497d03161a85406e755a32e3a6b73046
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-05 10:14:38 -0400

    NEXT-3: export Stats Guy market adapter

--- src/fsffl/value/sources/__init__.py
+++ src/fsffl/value/sources/__init__.py
@@ -1,12 +1,15 @@
 from .dynastydealer import DynastyDealerImportResult, normalize_dynastydealer_values
 from .dynastyprocess import DynastyProcessImportResult, normalize_dynastyprocess_values
 from .fantasycalc import FantasyCalcImportResult, normalize_fantasycalc_values
+from .statsguy import StatsGuyImportResult, normalize_statsguy_rankings
 
 __all__ = [
     "DynastyDealerImportResult",
     "DynastyProcessImportResult",
     "FantasyCalcImportResult",
+    "StatsGuyImportResult",
     "normalize_dynastydealer_values",
     "normalize_dynastyprocess_values",
     "normalize_fantasycalc_values",
+    "normalize_statsguy_rankings",
 ]
