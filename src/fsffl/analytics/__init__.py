c3acc9d5efa96a640be9e0096ec6f8f60b675bbc
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-05 14:17:13 -0400

    NEXT-7: export team analytics view

--- src/fsffl/analytics/__init__.py
+++ src/fsffl/analytics/__init__.py
@@ -5,11 +5,21 @@ from .models import (
     ModelLineageEntry,
     canonical_analytics_json,
 )
+from .team import (
+    DraftPickAnalyticsRow,
+    PlayerAnalyticsRow,
+    TeamAnalyticsView,
+    build_team_analytics_view,
+)
 
 __all__ = [
     "AnalyticsContext",
     "AnalyticsWarning",
     "AnalyticsWarningKind",
+    "DraftPickAnalyticsRow",
     "ModelLineageEntry",
+    "PlayerAnalyticsRow",
+    "TeamAnalyticsView",
+    "build_team_analytics_view",
     "canonical_analytics_json",
 ]
