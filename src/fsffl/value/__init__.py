81aa7c9d97923e29d271084fb5e9f96e7b953d82
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-13 04:09:57 -0400

    Export Intrinsic v1 Value bridge

--- src/fsffl/value/__init__.py
+++ src/fsffl/value/__init__.py
@@ -14,6 +14,7 @@ from .history import (
 )
 from .intrinsic import ForecastValueMapping, estimate_intrinsic_player_value
 from .intrinsic_v1 import (
+    INTRINSIC_VALUE_V1_SCALE,
     INTRINSIC_VALUE_V1_VERSION,
     INTRINSIC_VALUE_V1_WEIGHTS,
     REPLACEMENT_CONTEXT_VERSION,
@@ -21,6 +22,7 @@ from .intrinsic_v1 import (
     IntrinsicV1HorizonContribution,
     IntrinsicValueV1Estimate,
     ThreeValueCoordinates,
+    as_intrinsic_dynasty_value_estimate,
     build_marginal_lineup_replacement_paths,
     estimate_intrinsic_value_v1,
 )
@@ -30,10 +32,7 @@ from .market import (
     MarketObservation,
     estimate_market_price,
 )
-from .market_benchmark import (
-    MarketSourceBenchmarkResult,
-    benchmark_market_sources_against_transactions,
-)
+from .market_benchmark import MarketSourceBenchmarkResult, benchmark_market_sources_against_transactions
 from .market_context import MarketContextCalibration, apply_market_context
 from .market_context_fit import MarketContextFitPolicy, fit_market_context_calibration
 from .models import (
@@ -60,12 +59,7 @@ from .package_transaction_evidence import (
 from .pick import PickOutcome, PickOutcomeSet, estimate_pick_value
 from .source_batch import MarketSourceBatchResult, build_market_calibration_panel_batch
 from .source_catalog import next3_market_source_registry_v1
-from .source_registry import (
-    MarketSignalKind,
-    MarketSourceDefinition,
-    MarketSourceRegistry,
-    MarketSourceStatus,
-)
+from .source_registry import MarketSignalKind, MarketSourceDefinition, MarketSourceRegistry, MarketSourceStatus
 from .transaction import TransactionPriceMapping, estimate_transaction_price
 from .transaction_benchmark import (
     OneForOneEnsembleBenchmark,
@@ -80,71 +74,23 @@ from .transaction_evidence import (
 )
 
 __all__ = [
-    "AssetValueProfile",
-    "CalibrationEvidenceKind",
-    "CalibrationFitMetadata",
-    "CalibrationObservation",
-    "CalibrationPanel",
-    "DataRightsClass",
-    "ForecastValueInput",
-    "ForecastValueMapping",
-    "INTRINSIC_VALUE_V1_VERSION",
-    "INTRINSIC_VALUE_V1_WEIGHTS",
-    "IntrinsicDynastyValueEstimate",
-    "IntrinsicV1Confidence",
-    "IntrinsicV1HorizonContribution",
-    "IntrinsicValueV1Estimate",
-    "MarketBaselineMethod",
-    "MarketContextCalibration",
-    "MarketContextFitPolicy",
-    "MarketEvidenceKind",
-    "MarketMovement",
-    "MarketMovementStatus",
-    "MarketObservation",
-    "MarketPriceEstimate",
-    "MarketSignalKind",
-    "MarketSourceBatchResult",
-    "MarketSourceBenchmarkResult",
-    "MarketSourceDefinition",
-    "MarketSourceRegistry",
-    "MarketSourceStatus",
-    "MarketValueHistoryStore",
-    "MarketValueSnapshot",
-    "MultiAssetTradeObservation",
-    "OneForOneEnsembleBenchmark",
-    "OneForOneSourceBenchmark",
-    "OneForOneTradeBenchmarkResult",
-    "OneForOneTradeObservation",
-    "PackageAssetKind",
-    "PackageAssetLeg",
-    "PackageTradeSide",
-    "PickOutcome",
-    "PickOutcomeSet",
-    "PickValueEstimate",
-    "REPLACEMENT_CONTEXT_VERSION",
-    "SleeperOneForOneImportResult",
-    "SleeperPackageTradeImportResult",
-    "ThreeValueCoordinates",
-    "TransactionDirection",
-    "TransactionPriceEstimate",
-    "TransactionPriceMapping",
-    "ValueAssetKind",
-    "ValueDistribution",
-    "ValueScale",
-    "apply_market_context",
-    "benchmark_market_sources_against_one_for_one_trades",
-    "benchmark_market_sources_against_transactions",
-    "build_marginal_lineup_replacement_paths",
-    "build_market_calibration_panel_batch",
-    "calculate_market_movement",
-    "comparable_values",
-    "estimate_intrinsic_player_value",
-    "estimate_intrinsic_value_v1",
-    "estimate_market_price",
-    "estimate_pick_value",
-    "estimate_transaction_price",
-    "fit_market_context_calibration",
-    "next3_market_source_registry_v1",
-    "normalize_sleeper_one_for_one_trades",
-    "normalize_sleeper_package_trades",
+    "AssetValueProfile", "CalibrationEvidenceKind", "CalibrationFitMetadata", "CalibrationObservation",
+    "CalibrationPanel", "DataRightsClass", "ForecastValueInput", "ForecastValueMapping",
+    "INTRINSIC_VALUE_V1_SCALE", "INTRINSIC_VALUE_V1_VERSION", "INTRINSIC_VALUE_V1_WEIGHTS",
+    "IntrinsicDynastyValueEstimate", "IntrinsicV1Confidence", "IntrinsicV1HorizonContribution",
+    "IntrinsicValueV1Estimate", "MarketBaselineMethod", "MarketContextCalibration", "MarketContextFitPolicy",
+    "MarketEvidenceKind", "MarketMovement", "MarketMovementStatus", "MarketObservation", "MarketPriceEstimate",
+    "MarketSignalKind", "MarketSourceBatchResult", "MarketSourceBenchmarkResult", "MarketSourceDefinition",
+    "MarketSourceRegistry", "MarketSourceStatus", "MarketValueHistoryStore", "MarketValueSnapshot",
+    "MultiAssetTradeObservation", "OneForOneEnsembleBenchmark", "OneForOneSourceBenchmark",
+    "OneForOneTradeBenchmarkResult", "OneForOneTradeObservation", "PackageAssetKind", "PackageAssetLeg",
+    "PackageTradeSide", "PickOutcome", "PickOutcomeSet", "PickValueEstimate", "REPLACEMENT_CONTEXT_VERSION",
+    "SleeperOneForOneImportResult", "SleeperPackageTradeImportResult", "ThreeValueCoordinates",
+    "TransactionDirection", "TransactionPriceEstimate", "TransactionPriceMapping", "ValueAssetKind",
+    "ValueDistribution", "ValueScale", "apply_market_context", "as_intrinsic_dynasty_value_estimate",
+    "benchmark_market_sources_against_one_for_one_trades", "benchmark_market_sources_against_transactions",
+    "build_marginal_lineup_replacement_paths", "build_market_calibration_panel_batch", "calculate_market_movement",
+    "comparable_values", "estimate_intrinsic_player_value", "estimate_intrinsic_value_v1", "estimate_market_price",
+    "estimate_pick_value", "estimate_transaction_price", "fit_market_context_calibration",
+    "next3_market_source_registry_v1", "normalize_sleeper_one_for_one_trades", "normalize_sleeper_package_trades",
 ]
