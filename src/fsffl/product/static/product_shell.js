1931ae7f3d527880f8583575ef9269e2783522c6
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-23 11:14:50 -0400

    chore: bust League Atlas iPhone polish assets

--- src/fsffl/product/static/product_shell.js
+++ src/fsffl/product/static/product_shell.js
@@ -25,7 +25,7 @@ const fsfflProductSurfaceCopy={
 };
 
 const fsfflStaticVersion='20260920-owner-dossier1';
-const leagueAtlasStaticVersion='20260923-league-atlas-final1';
+const leagueAtlasStaticVersion='20260923-league-atlas-iphone1';
 let leagueComparisonScriptPromise=null;
 let myTeamScriptPromise=null;
 let reportsScriptPromise=null;
