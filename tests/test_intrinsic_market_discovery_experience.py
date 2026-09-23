2a11786cac5f9b58d1337c27b31741e58006163c
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-23 18:19:10 -0400

    test: advance hosted release token for Home

--- tests/test_intrinsic_market_discovery_experience.py
+++ tests/test_intrinsic_market_discovery_experience.py
@@ -10,7 +10,7 @@ import pytest
 
 STATIC = Path(__file__).resolve().parents[1] / "src" / "fsffl" / "product" / "static"
 INDEX = STATIC / "index.html"
-RELEASE = "20260913-phase3-latency1"
+RELEASE = "20260923-home-north-star1"
 
 
 def _text(name: str) -> str:
