a6d48b9bba5e01ff9dcf02c2209273b9d6c6aed6
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-24 11:28:02 -0400

    Align North Star release regression with hotfix

--- tests/test_market_focus_and_trade_drilldown.py
+++ tests/test_market_focus_and_trade_drilldown.py
@@ -9,7 +9,7 @@ STATIC = ROOT / "static"
 FOCUS_JS = STATIC / "market_focus_server.js"
 DRILLDOWN_JS = STATIC / "market_trade_drilldown.js"
 INDEX = STATIC / "index.html"
-RELEASE = "20260924-market-north-star1"
+RELEASE = "20260924-live-usability-hotfix1"
 
 
 def _read(path: Path) -> str:
