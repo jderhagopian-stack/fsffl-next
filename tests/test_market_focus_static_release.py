1d423f8c0e3a88e9dc5c7dbbe3aded473501ab75
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-23 20:41:53 -0400

    test: advance coherent Home readiness release token

--- tests/test_market_focus_static_release.py
+++ tests/test_market_focus_static_release.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 INDEX = Path("src/fsffl/product/static/index.html")
-RELEASE = "20260923-home-north-star1"
+RELEASE = "20260924-home-readiness1"
 
 
 def test_focus_and_drilldown_share_current_release_token() -> None:
