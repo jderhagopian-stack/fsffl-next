af6982e53149da6807d04ea89a9b17148218c65a
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-23 20:44:04 -0400

    test: advance hosted release token for Home readiness

--- tests/test_market_focus_and_trade_drilldown.py
+++ tests/test_market_focus_and_trade_drilldown.py
@@ -9,7 +9,7 @@ STATIC = ROOT / "static"
 FOCUS_JS = STATIC / "market_focus_server.js"
 DRILLDOWN_JS = STATIC / "market_trade_drilldown.js"
 INDEX = STATIC / "index.html"
-RELEASE = "20260923-home-north-star1"
+RELEASE = "20260924-home-readiness1"
 
 
 def _read(path: Path) -> str:
