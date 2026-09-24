fa7e8ab125ab769b0f1f463871d68044a250d290
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-23 20:44:18 -0400

    test: advance hosted release token for Home readiness

--- tests/test_north_star_trade_center.py
+++ tests/test_north_star_trade_center.py
@@ -5,7 +5,7 @@ SCRIPT = ROOT / "north_star_trade_center.js"
 CSS = ROOT / "north_star_trade_center.css"
 INDEX = ROOT / "index.html"
 HANDOFF = ROOT / "trade_workflow_handoff.js"
-RELEASE = "20260923-home-north-star1"
+RELEASE = "20260924-home-readiness1"
 
 def _read(path: Path) -> str:
     return path.read_text(encoding="utf-8")
