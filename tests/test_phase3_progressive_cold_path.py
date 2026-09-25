b2f1c048e646c6c61afa4012e5411b33b4fe8b31
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-25 14:30:48 -0400

    Align static cache assertions with Hodor lifecycle release

--- tests/test_phase3_progressive_cold_path.py
+++ tests/test_phase3_progressive_cold_path.py
@@ -15,7 +15,7 @@ from fsffl.product import scenario_cache
 ROOT = Path(__file__).resolve().parents[1]
 PRODUCT = ROOT / "src" / "fsffl" / "product"
 STATIC = PRODUCT / "static"
-RELEASE = "20260925-market-beta-corrective2"
+RELEASE = "20260925-hodor-lifecycle1"
 
 
 def _text(path: Path) -> str:
