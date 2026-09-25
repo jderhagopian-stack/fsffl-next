2c8f2c4c882bb551548fb547967f9a7413a13967
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-25 07:55:56 -0400

    Align static release token with last-good repair

--- tests/test_intrinsic_market_discovery_experience.py
+++ tests/test_intrinsic_market_discovery_experience.py
@@ -10,7 +10,7 @@ import pytest
 
 STATIC = Path(__file__).resolve().parents[1] / "src" / "fsffl" / "product" / "static"
 INDEX = STATIC / "index.html"
-RELEASE = "20260925-market-discovery1"
+RELEASE = "20260925-lastgood-repair1"
 
 
 def _text(name: str) -> str:
