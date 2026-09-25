ad8dc7a4412dff9fbd870f8888f1086bc1000466
jderhagopian-stack <j.derhagopian@gmail.com>
2026-09-24 21:15:46 -0400

    Advance hosted asset generation to control7

--- tests/test_market_trade_recomposition.py
+++ tests/test_market_trade_recomposition.py
@@ -4,7 +4,7 @@ ROOT = Path("src/fsffl/product/static")
 SCRIPT = ROOT / "market_trade_recomposition.js"
 CSS = ROOT / "market_trade_recomposition.css"
 INDEX = ROOT / "index.html"
-RELEASE = "20260924-readiness-control6"
+RELEASE = "20260924-readiness-control7"
 
 
 def _read(path: Path) -> str:
